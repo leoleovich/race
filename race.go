dc5b20110d269111f1a89b8b5d1d8fad7beaf75e
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2017-04-17 12:34:07 +0200

    Print the score

--- race.go
+++ race.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
+	"github.com/pkg/errors"
 	"log"
 	"math/rand"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -13,14 +17,23 @@ const road_width = 40
 const road_lenght = 20
 const car_width = 16
 const car_lenght = 7
+const result_width = 75
 
 type Config struct {
-	Log      *log.Logger
-	AcidPath string
+	Log                 *log.Logger
+	AcidPath, ScorePath string
 }
 
 type Point struct {
 	X, Y int
+}
+
+type GameData struct {
+	playerName                string
+	carPosition, bombPosition Point
+	roads                     [][]byte
+	car, clear, gameOver      []byte
+	gameStarted               int64
 }
 
 func generateRoad(reverse bool) []byte {
@@ -95,56 +108,105 @@ func updatePosition(conn net.Conn, position *Point) {
 	}
 }
 
+func readName(conf *Config, conn net.Conn) (string, error) {
+	conn.Write([]byte("Enter your name:"))
+	io := bufio.NewReader(conn)
+	name, err := io.ReadString('\n')
+	if err != nil {
+		conf.Log.Println("Error while name reading", err)
+		return "", err
+	}
+	if name == "" {
+		conf.Log.Println("Empty name")
+		return "", errors.New("Empty name")
+	}
+	if len(name) > result_width/2 {
+		conf.Log.Println("Too long name")
+		return "", errors.New("Too long name")
+	}
+	return strings.Replace(name, "\n", "", -1), nil
+}
+
+func gameOver(conf *Config, conn net.Conn, gameData *GameData) {
+	diff := fmt.Sprintf("%d", time.Now().Unix()-gameData.gameStarted)
+
+	//Name
+	for i, char := range []byte(gameData.playerName) {
+		gameData.gameOver[i] = char
+	}
+	//:
+	gameData.gameOver[result_width/2] = byte(':')
+	// Score
+	for i := range diff {
+		gameData.gameOver[result_width-len(diff)+i] = byte(diff[i])
+
+	}
+	conn.Write(gameData.clear)
+	conn.Write(gameData.gameOver)
+
+}
+
 func handleRequest(conf *Config, conn net.Conn) {
 	defer conn.Close()
 
-	carPosition := Point{12, 12}
-	bombPosition := Point{road_width, road_lenght}
+	gameData := GameData{}
+	gameData.carPosition = Point{12, 12}
+	gameData.bombPosition = Point{road_width, road_lenght}
 
-	roads := [][]byte{generateRoad(false), generateRoad(true)}
-	car := getAcid(conf, "car.txt")
-	clear := getAcid(conf, "clear.txt")
-	gameOver := getAcid(conf, "game_over.txt")
+	gameData.roads = [][]byte{generateRoad(false), generateRoad(true)}
+	gameData.car = getAcid(conf, "car.txt")
+	gameData.clear = getAcid(conf, "clear.txt")
+	gameData.gameOver = getAcid(conf, "game_over.txt")
 
-	go updatePosition(conn, &carPosition)
+	name, err := readName(conf, conn)
+	if err != nil {
+		return
+	}
+	gameData.playerName = name
+	gameData.gameStarted = time.Now().Unix()
+
+	go updatePosition(conn, &gameData.carPosition)
+
 	for {
-		if carPosition.X < 1 || carPosition.X > 23 || carPosition.Y < 1 || carPosition.Y > 12 {
+		if gameData.carPosition.X < 1 || gameData.carPosition.X > 23 || gameData.carPosition.Y < 1 || gameData.carPosition.Y > 12 {
 			// Hit the wall
-			conn.Write(gameOver)
+			gameOver(conf, conn, &gameData)
 			return
-		} else if carPosition.X <= bombPosition.X && carPosition.X+car_width-1 > bombPosition.X &&
-			carPosition.Y < bombPosition.Y && carPosition.Y+car_lenght-1 > bombPosition.Y {
+		} else if gameData.carPosition.X <= gameData.bombPosition.X && gameData.carPosition.X+car_width-1 > gameData.bombPosition.X &&
+			gameData.carPosition.Y < gameData.bombPosition.Y && gameData.carPosition.Y+car_lenght-1 > gameData.bombPosition.Y {
 			// Hit the bomb
-			conn.Write(gameOver)
+			gameOver(conf, conn, &gameData)
 			return
 		}
 
-		for i := range roads {
-			data := make([]byte, len(roads[i]))
-			copy(data, roads[i])
+		for i := range gameData.roads {
+			data := make([]byte, len(gameData.roads[i]))
+			copy(data, gameData.roads[i])
+
+			// Moving cursor at the beginning
+			_, err := conn.Write(gameData.clear)
+			if err != nil {
+				return
+			}
 
 			// Applying the bomb
-			if bombPosition.Y < road_lenght {
-				data[bombPosition.Y*road_width+bombPosition.X] = byte('X')
-				bombPosition.Y++
+			if gameData.bombPosition.Y < road_lenght {
+				data[gameData.bombPosition.Y*road_width+gameData.bombPosition.X] = byte('X')
+				gameData.bombPosition.Y++
 			} else if rand.Int()%3 == 0 {
-				bombPosition.X, bombPosition.Y = rand.Intn(road_width-3)+1, 0
+				gameData.bombPosition.X, gameData.bombPosition.Y = rand.Intn(road_width-3)+1, 0
 			}
 
 			// Applying the car
 			for line := 0; line < 7; line++ {
-				copy(data[((carPosition.Y+line)*road_width+carPosition.X):((carPosition.Y+line)*road_width+carPosition.X)+15], car[line*car_width:line*car_width+15])
+				copy(data[((gameData.carPosition.Y+line)*road_width+gameData.carPosition.X):((gameData.carPosition.Y+line)*road_width+gameData.carPosition.X)+15], gameData.car[line*car_width:line*car_width+15])
 			}
 
-			_, err := conn.Write(data)
+			_, err = conn.Write(data)
 			if err != nil {
 				return
 			}
 			time.Sleep(200 * time.Millisecond)
-			_, err = conn.Write(clear)
-			if err != nil {
-				return
-			}
 		}
 	}
 }
@@ -154,7 +216,8 @@ func main() {
 	conf := &Config{}
 
 	flag.StringVar(&logFile, "l", "/var/log/race.log", "Log file")
-	flag.StringVar(&conf.AcidPath, "s", "/Users/leoleovich/go/src/github.com/leoleovich/race/artifacts", "Story file")
+	flag.StringVar(&conf.AcidPath, "a", "/Users/leoleovich/go/src/github.com/leoleovich/race/artifacts", "Artifacts location")
+	flag.StringVar(&conf.AcidPath, "s", "/Users/leoleovich/go/src/github.com/leoleovich/race/artifacts", "Score location")
 	flag.Parse()
 
 	logfile, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
